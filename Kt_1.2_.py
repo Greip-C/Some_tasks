537d9b8993782b955b408c5e81d93bb8c31a5303
Greip-C <timurburenkov@gmail.com>
2025-03-29 18:23:10 +0200

    Update Kt_1.2_.py

--- Kt_1.2_.py
+++ Kt_1.2_.py
@@ -1,6 +1,42 @@
 import math
 
-def variant_17_2(a,b,n,d):
+def variant_17_2():
+
+    def input_check_f (promt): # Base input check float
+            while True:
+                try:
+                    return float(input(promt))
+                except ValueError:
+                    print("Sisestamise viga!")
+                    ##sys.exit()
+
+    def input_check_i (promt): # Base input check int
+            while True:
+                try:
+                    return int(input(promt))
+                except ValueError:
+                    print("Sisestamise viga!")
+                    ##sys.exit()
+
+    def input_comparing_f (): # Input comparing a and b
+        def input_check (promt): # Base input check
+            while True:
+                try:
+                    return float(input(promt))
+                except ValueError:
+                    print("Sisestamise viga!")
+                    ##sys.exit()
+
+        while True:
+            Start = input_check("Sisesta vahemiku algus => ")
+            End = input_check("Sisesta vahemiku lõpp => ")
+            if Start < End:
+                break
+            else:
+                print("Valed andmed")
+                continue
+                ##sys.exit()
+        return(Start,End)
 
     def n_sort(a,b,n): #Point generator
         step = (b-a)/(n)
@@ -23,12 +59,17 @@ def variant_17_2(a,b,n,d):
     def tuple_round(a): #Make tuple rounded
         return tuple(round(i,2) for i in a)
 
-    def d_comparing(t_input,d):
+    def d_comparing(t_input,d): #d comparing with Y(x)
         count =0
         for i in range(len(t_input)):
             if t_input[i] < d:
                 count = count+1
         return count
+
+    a,b=input_comparing_f()
+    print("AAAAAAAAAAAAAA")
+    n=input_check_i("Sisesta jaotiste arv => ")
+    d=input_check_f("Sisesta mingi reaalarv => ")
 
     points=n_sort(a,b,n)
 
@@ -42,9 +83,11 @@ def variant_17_2(a,b,n,d):
     #print(r_points)
     #print(r_y_output)
 
-    for i in range(n):
+    for i in range(n+1):
         print(f"{r_points[i]},\t{r_y_output[i]}")
     
     print(d_comparing(r_y_output,d))
 
-variant_17_2(-2.5,2.5,12,0.5)
+
+
+variant_17_2()
